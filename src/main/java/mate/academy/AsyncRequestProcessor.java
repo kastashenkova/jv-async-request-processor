caadb157e370ffc70b2ebc12d493b276ecc42556
kastashenkova <astashenkova.katya@gmail.com>
2026-03-17 23:02:01 +0200

    develop asynchronous request

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,9 +1,12 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
+    private static final Map<String, UserData> cache = new ConcurrentHashMap<>();
     private final Executor executor;
 
     public AsyncRequestProcessor(Executor executor) {
@@ -11,6 +14,18 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> getUserData(userId),
+                executor);
+    }
+
+    private static UserData getUserData(String userId) {
+        UserData res;
+        if (cache.get(userId) != null) {
+            res = cache.get(userId);
+        } else {
+            res = new UserData(userId, "Details for " + userId);
+            cache.put(userId, res);
+        }
+        return res;
     }
 }
